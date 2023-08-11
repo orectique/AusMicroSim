aae8b651e751a6d7eae5cdda4d3a364e37923ee3
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2023-08-11 11:54:18 +1000

    upd

--- Scripts and Notebooks/microSim.R
+++ Scripts and Notebooks/microSim.R
@@ -1,5 +1,47 @@
+library(data.table)
+setDTthreads(10)
+
 library(simPop)
 library(readr)
 
-sample_file <- read_csv("./Data Files/2020_sample_file.csv")
+library(dplyr)
+
+sample_file <- data.table(read_csv("./Data Files/microdata.csv"))
+
+sample_file <- filter(sample_file, Region == 0)
+
+constraints <- data.table(read_csv("./Data Files/constraintsAus.csv"))
+
+constraints <- filter(constraints, Region == 0)
+
+sample_file$hsize <- 1
+sample_file$weights <- 50
+
+sample_file$Gender <- as.factor(sample_file$Gender)
+sample_file$age_range <- as.factor(sample_file$age_range)
+sample_file$Region <- as.factor(sample_file$Region)
+sample_file$Income <- as.factor(sample_file$Income)
+
+colnames(constraints) <- c("Income", "Gender", "age_range", "Postal", "Freq", "Region")
+
+constraints$Gender <- as.factor(constraints$Gender)
+constraints$age_range <- as.factor(constraints$age_range)
+constraints$Region <- as.factor(constraints$Region)
+constraints$Income <- as.factor(constraints$Income)
+constraints$Postal <- as.factor(constraints$Postal)
+
+inp <- specifyInput(sample_file, hhid="Ind", hhsize="hsize", pid="Ind", strata="Region", weight="weights")
+
+print(inp)
+
+library(dplyr)
+
+constraints.regional <- constraints %>% 
+  group_by(Gender, age_range, Region, Income) %>%
+  summarise(Freq = sum(Freq))
+
+constraints.regional = data.frame(constraints.regional)
+
+addWeights(inp) <- calibSample(inp, constraints.regional, nr_cpus = 10)
+
 
--- Scripts and Notebooks/samplePrep.R
+++
@@ -1,3 +0,0 @@
-library(readr)
-
-sample_file <- read_csv("./Data Files/2020_sample_file.csv")
